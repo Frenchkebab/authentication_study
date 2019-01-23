3be835944dd4d33dd8e809284b9a18e7c88317da
frenchkebab <tubowangin@naver.com>
2019-01-23 14:12:08 +0900

    blogs/templates에 layout, post_list, post_detail 작성

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 # authentication/urls.py
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('', lambda req: redirect('/blog/')) # URL reverse
 ]
