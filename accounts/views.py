383b8166721c05cfb3a5b07e373282c565091c16
frenchkebab <tubowangin@naver.com>
2019-01-23 23:15:04 +0900

    SinupForm이 UserCreationForm을 상속하도록 수정

--- accounts/views.py
+++ accounts/views.py
@@ -5,23 +5,24 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.views.generic import CreateView
 from django.shortcuts import redirect, render
+from .forms import SignupForm
 
 # 방법1 : FBV
 """ def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             return redirect(settings.LOGIN_URL)
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'accounts/signup.html', {
         'form' : form,
     }) """
 
 # 방법2 : CBV
 signup = CreateView.as_view(model=User, 
-form_class=UserCreationForm,
+form_class=SignupForm,
 success_url=settings.LOGIN_URL,
 template_name='accounts/signup.html')    
 
